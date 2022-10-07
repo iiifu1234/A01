c239c315492c8068fffe3f844206f0cd727b5c2f
Patrick Chen <iiifu1234@gmail.com>
2022-10-07 17:09:12 +0800

    1. 新增talbe表設計    - 可在最後一項放入百分比    - 修改可以讓字體閃爍(尚未完成) 2. 新增timer計時器

--- UI-PyQt/UI_controller.py
+++ UI-PyQt/UI_controller.py
@@ -1,17 +1,20 @@
 
 import time
-
+from PyQt5.QtWidgets import *
 from PyQt5 import QtWidgets, QtGui, QtCore
 from PyQt5.QtWidgets import QFileDialog
 from UI import Ui_MainWindow
 
 class MainWindow_controller(QtWidgets.QMainWindow):
+
     def __init__(self):
         # in python3, super(Clpiass, self).xxx = super().xxx
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.UI_variable_manage()  # 與UI變數管理宣告
         self.setup_control()
+        self.count = 0
 
     def UI_variable_manage(self):
         #-----Button 管理區-----
@@ -23,18 +26,96 @@ class MainWindow_controller(QtWidgets.QMainWindow):
         self.ls_file_test_item = self.ui.ls_fileTestItem
         self.ls_select_test_item = self.ui.ls_selectTestItem
 
-
+        #-----table 管理區-----
+        self.testtable = self.ui.testTable
 
     def setup_control(self):
-        self.UI_variable_manage()      # 與UI變數管理宣告
         self.ls_select_test_item.setVisible(False)
         self.btn_selectTest.setVisible(False)
         # self.ui.pushButton.setVisible(False)
         self.ui.centralwidget.setWindowTitle('List Test')
         self.btn_selectTest.clicked.connect(self.btn_sendto)
         self.btn_openFile.clicked.connect(self.open_file)
-        self.test_item()
+        self.btn_gotoTest.clicked.connect(self.goto_test)
 
+        self.test_item()
+        self.testtable_init_fun()
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.run)
+        self.timer.start(50)
+        self.time_counter = 0
+
+    def run(self):
+        self.ui.label.setText(str(self.time_counter)) # show time_counter (by format)
+        self.time_counter += 1 # time_counter + 1
+
+    def testtable_init_fun(self):
+        newItem=QTableWidgetItem('1')
+        self.testtable.setItem(0,0,newItem)
+
+        newItem=QTableWidgetItem('Test1')
+        self.testtable.setItem(0,1,newItem)
+
+        newItem=QTableWidgetItem('ask idn')
+        self.testtable.setItem(0,2,newItem)
+
+        #设置表格头为伸缩模式
+        self.testtable.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+
+        # 优化3：將表格設置為禁止編輯模式
+        self.testtable.setEditTriggers(QAbstractItemView.NoEditTriggers)
+
+        #优化4：表格整行选中，表格默认选择的是单个单元格
+        self.testtable.setSelectionBehavior(QAbstractItemView.SelectRows)
+
+        # 优化5：将行与列的宽度高度与文本内容的宽高相匹配
+        QTableWidget.resizeColumnsToContents(self.testtable)
+        QTableWidget.resizeRowsToContents(self.testtable)
+
+        # 优化6：表格头的显示与隐藏
+        self.testtable.verticalHeader().setVisible(False)
+        # self.testtable.horizontalHeader().setVisible(False)
+
+    def goto_test(self):
+        # 优化7：在单元格内放置控件
+        # ↓放置進度條↓
+        style = '''
+            QProgressBar {
+                border: 2px solid #000;
+                border-radius: 5px;
+                text-align:center;
+                height: 20px;
+                width:200px;
+            }
+            QProgressBar::chunk {
+                background: #09c;
+                width:1px;
+            }
+        '''
+        # gressbar = QProgressBar()
+        # gressbar.setStyleSheet('QProgressBar{margin:3px}')
+        # self.testtable.setStyleSheet(style)
+        # self.gressbar_select(self.count,3,gressbar)
+        # self.count +=1
+
+        # ↓放入文字↓
+        label = QLabel()
+        self.label_setText(self.count,3,label)
+
+    def label_setText(self,row,col,label):
+        label.setText('Wait')
+        label.setStyleSheet('color: green')
+        label.setAlignment(QtCore.Qt.AlignCenter)
+        self.testtable.setCellWidget(row, col, label)
+        # self.time_counter <100
+        label.setVisible(False)
+    def gressbar_select(self,row,col,gressbar):
+        self.testtable.setCellWidget(row, col, gressbar)
+        max_value = 100
+        gressbar.setMaximum(max_value)
+        for i in range(max_value):
+            gressbar.setValue(i + 1)
+            time.sleep(0.01)
     def open_file(self):
         """
         可以直接使用 QFileDialog.getOpenFileName 這個已經設定好的函式，
